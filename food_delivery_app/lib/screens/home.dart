5eb96edd138a855944f548cc12860b001be778c0
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-11-28 14:00:08 +0530

    Home Widgets
    Row and Column widgets were added to the home page.

--- food_delivery_app/lib/screens/home.dart
+++ food_delivery_app/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/main.dart';
 
 class Home extends StatefulWidget {
   const Home({ Key? key }) : super(key: key);
@@ -12,8 +13,62 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hello'),
-      ),      
+        backgroundColor: Colors.amber,        
+        title: const Text('Hello, Peter!'),
+        actions: [
+          IconButton(
+            onPressed: () async {},
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+            )
+          ),
+          Form(
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              decoration: const BoxDecoration(color: Colors.white)
+            )
+          ),
+        ],
+      ),   
+      body: const Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 15,
+        ),
+        child: SafeArea(child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Explore Foods",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text(
+                    "View All",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.amber,
+                    ),  
+                  ),
+                ] 
+              ),
+              Row(),
+              Column(),
+              // TextButton(
+              //   onPressed: () { },                
+              //),
+            ],
+          ),        
+        )
+      ),               
+      ),   
     );
   }
 } 
