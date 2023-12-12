2a54f6168791c1e864a147a426a1625f99df2f23
Teshan2000 <teshan_wijewardhana@outlook.com>
2023-12-12 15:19:20 +0530

    Ingredients List
    Ingredients list were added to the food details page.

--- food_delivery_app/lib/screens/foodDetails.dart
+++ food_delivery_app/lib/screens/foodDetails.dart
@@ -8,6 +8,13 @@ class FoodDetails extends StatefulWidget {
 }
 
 class FoodDetailsState extends State<FoodDetails> {
+  List<Map<String, dynamic>> ingredients = [
+    {"icon": Icons.breakfast_dining, "name": "Bread"},
+    {"icon": Icons.child_care, "name": "Chicken"},
+    {"icon": Icons.play_arrow, "name": "Cheese"},
+    {"icon": Icons.circle, "name": "Tomato"},
+    {"icon": Icons.forest, "name": "Salad"}
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -51,6 +58,57 @@ class FoodDetailsState extends State<FoodDetails> {
                 style: TextStyle(fontSize: 18),
               ),
             ]),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              height: 140,
+              width: double.infinity,
+              child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: List.generate(ingredients.length, (index) {
+                    return Flexible(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 2),
+                        child: Container(
+                          width: 75,
+                          height: 100,
+                          decoration: ShapeDecoration(
+                            color: const Color(0xFFFFC107),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(90),
+                            ),
+                          ),
+                          child: Column(children: [
+                            Flexible(
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 3, vertical: 2),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: <Widget>[
+                                    // Image.asset(
+                                    //   ingredients[index]['image'],
+                                    //   width: 60,
+                                    //   height: 60,
+                                    // ),
+                                    Icon(
+                                      ingredients[index]['icon'],
+                                      color: Colors.white,
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      ingredients[index]['name'],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ]),
+                        ),
+                      ),
+                    );
+                  })),
+            ),
             const SizedBox(height: 8),
             Container(
               padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -66,45 +124,69 @@ class FoodDetailsState extends State<FoodDetails> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          '-',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        '-',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white                    
                         ),
-                        Text(
-                          'Rs. 95.00',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                      Text(
+                        'Rs. 95.00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white                    
                         ),
-                        Text(
-                          '+',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                      Text(
+                        '+',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white                    
                         ),
-                      ]),
+                      ),
+                    ]
+                  ),
                   Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          'Chicken Burger',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Chicken Burger',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white                    
                         ),
-                        Text(
-                          '2',
-                          style: TextStyle(fontSize: 18, color: Colors.white),
-                        )
-                      ]),
+                      ),
+                      Text(
+                        '2',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white                    
+                        ),
+                      )
+                    ]
+                  ),
                   Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          'Total',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Total',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white                    
                         ),
-                        Text(
-                          'Rs. 190.00',
-                          style: TextStyle(fontSize: 16, color: Colors.white),
-                        )
-                      ])
+                      ),
+                      Text(
+                        'Rs. 190.00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white                    
+                        ),
+                      )
+                    ]
+                  )
                 ],
               ),
             ),
@@ -112,6 +194,13 @@ class FoodDetailsState extends State<FoodDetails> {
             Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
               ElevatedButton(
                 style: style,
+                // style: ButtonStyle(
+                //   backgroundColor: Colors.amber,
+                //   textStyle: TextStyle(
+                //     color: Colors.white,
+                //     fontSize: 18
+                //   )
+                // ),
                 //style: style,
                 // style: ButtonStyle(
                 //   textStyle: TextStyle(
