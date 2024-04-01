d42bd811bcf1be4b99aa64aa223d36d2cd803e32
Teshan2000 <teshan_wijewardhana@outlook.com>
2024-04-02 00:55:51 +0530

    Cart Updated
    Cart page was updated with new quantity buttons.

--- food_delivery_app/lib/screens/cart.dart
+++ food_delivery_app/lib/screens/cart.dart
@@ -1,5 +1,7 @@
+import 'package:cart_stepper/cart_stepper.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/screens/foodDetails.dart';
+import 'package:input_quantity/input_quantity.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -9,6 +11,19 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+  int quantity = 1;
+  double totalPrice = 95.00;
+
+  int _counterInit = 0;
+  int _counter = 1;
+  int _counterLimit = 1;
+  double _dCounter = 0.01;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   List<Map<String, dynamic>> cart = [
     {
       "image": "Assets/Foods/Chicken Burger.png",
@@ -27,9 +42,15 @@ class _CartState extends State<Cart> {
     }
   ];
 
+  void onQtyChanged(int newQuantity) {
+    setState(() {
+      quantity = newQuantity;
+      totalPrice = 95.00 * quantity;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     final ButtonStyle style = ElevatedButton.styleFrom(
         backgroundColor: Colors.amber,
         elevation: 5,
@@ -62,7 +83,8 @@ class _CartState extends State<Cart> {
               Column(
                 children: [
                   Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
                     height: 375,
                     child: GestureDetector(
                       child: ListView(
@@ -74,42 +96,106 @@ class _CartState extends State<Cart> {
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(30),
                               ),
-                              child: Row(children: [
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 5, vertical: 15),
-                                  child: Row(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: <Widget>[
-                                        const SizedBox(width: 20),
-                                        Image.asset(
-                                          cart[index]['image'],
-                                          width: 80,
-                                          height: 80,
-                                        ),
-                                        const SizedBox(width: 40),
-                                        Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: <Widget>[
-                                              Text(
-                                                cart[index]['name'],
-                                                style: const TextStyle(
-                                                    fontSize: 18,
-                                                    color: Colors.white),
-                                              ),
-                                              Text(
-                                                cart[index]['price'],
-                                                style: const TextStyle(
-                                                    fontSize: 18,
-                                                    color: Colors.white),
-                                              )
-                                            ])
-                                      ]),
-                                ),
-                              ]),
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 5, vertical: 15),
+                                      child: Row(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
+                                          children: <Widget>[
+                                            const SizedBox(width: 20),
+                                            Image.asset(
+                                              cart[index]['image'],
+                                              width: 80,
+                                              height: 80,
+                                            ),
+                                            const SizedBox(width: 25),
+                                            Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: <Widget>[
+                                                  Text(
+                                                    cart[index]['name'],
+                                                    style: const TextStyle(
+                                                        fontSize: 18,
+                                                        color: Colors.white),
+                                                  ),
+                                                  Text(
+                                                    cart[index]['price'],
+                                                    style: const TextStyle(
+                                                        fontSize: 18,
+                                                        color: Colors.white),
+                                                  )
+                                                ]),
+                                            // Container(
+                                            //   width: 50,
+                                            //   height: 80,
+                                            //   decoration: ShapeDecoration(
+                                            //     color: const Color.fromARGB(255, 255, 255, 255),
+                                            //     shape: RoundedRectangleBorder(
+                                            //       borderRadius: BorderRadius.circular(90),
+                                            //     ),
+                                            //   ),
+                                            //   child: InputQty.int(
+                                            //     qtyFormProps: const QtyFormProps(cursorColor: Colors.amber),
+                                            //     decoration: const QtyDecorationProps(
+                                            //       width: 12,
+                                            //       orientation: ButtonOrientation.vertical,
+                                            //       isBordered: false,
+                                            //       borderShape: BorderShapeBtn.circle,
+                                            //     ),
+                                            //     onQtyChanged: (newQty) {
+                                            //       onQtyChanged(newQty);
+                                            //     },
+                                            //   ),
+                                            // ),
+                                            //                         Row(
+                                            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            //   children: [
+                                            //     InputQty.int(
+                                            //       messageBuilder: (minVal, maxVal, value) => const Text(
+                                            //           "Button on Left",
+                                            //           textAlign: TextAlign.center),
+                                            //       decoration: const QtyDecorationProps(
+                                            //           // qtyStyle: QtyStyle.btnOnLeft,
+                                            //           orientation: ButtonOrientation.vertical,
+                                            //           width: 4,
+                                            //           btnColor: Colors.white,
+                                            //           fillColor: Colors.black12,
+                                            //           isBordered: false,
+                                            //           borderShape: BorderShapeBtn.circle
+                                            //         ),
+                                            //     ),
+                                            //   ],
+                                            // ),
+                                            const SizedBox(width: 25),
+                                            CartStepperInt(
+                                              style: const CartStepperStyle(
+                                                  iconTheme: IconThemeData(
+                                                      color: Colors.black),
+                                                  foregroundColor: Colors.black,
+                                                  activeForegroundColor:
+                                                      Colors.black,
+                                                  backgroundColor: Colors.white,
+                                                  activeBackgroundColor:
+                                                      Colors.white),
+                                              value: _counter,
+                                              axis: Axis.vertical,
+                                              didChangeCount: (count) {
+                                                setState(() {
+                                                  _counter = count;
+                                                });
+                                              },
+                                            ),
+                                            const SizedBox(width: 15),
+                                          ]),
+                                    ),
+                                  ]),
                             );
                           })),
                       onTap: () {
@@ -132,19 +218,20 @@ class _CartState extends State<Cart> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      const Row(
+                      Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
-                            Text(
+                            const Text(
                               'Sub Total',
                               style:
                                   TextStyle(fontSize: 16, color: Colors.black),
                             ),
-                            SizedBox(height: 40, width: 180),
+                            const SizedBox(height: 40, width: 170),
                             Text(
-                              'Rs. 255.00',
-                              style:
-                                  TextStyle(fontSize: 16, color: Colors.black),
+                              // 'Rs. 255.00',
+                              'Rs. $totalPrice''0',
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.black),
                             ),
                           ]),
                       const Row(
@@ -162,22 +249,12 @@ class _CartState extends State<Cart> {
                                   TextStyle(fontSize: 16, color: Colors.black),
                             ),
                           ]),
-                      const SizedBox(height: 3, width: 180),
-                      Row(children: <Widget>[
-                        const SizedBox(height: 20),
-                        Container(
-                          width: 330,
-                          height: 3,
-                          decoration: ShapeDecoration(
-                            color: const Color.fromARGB(0, 0, 0, 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(90),
-                              side: const BorderSide(),
-                            ),
-                          ),
-                        ),
-                      ]),
-                      const Row(
+                      const SizedBox(height: 3, width: 180),                      
+                      const Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                      Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
                             Text(
@@ -187,7 +264,8 @@ class _CartState extends State<Cart> {
                             ),
                             SizedBox(height: 40, width: 180),
                             Text(
-                              'Rs. 305.00',
+                              // 'Rs. 305.00',
+                              'Rs. $totalPrice''0',
                               style:
                                   TextStyle(fontSize: 16, color: Colors.black),
                             ),
--- food_delivery_app/lib/screens/home.dart
+++ food_delivery_app/lib/screens/home.dart
@@ -94,7 +94,7 @@ class _HomeState extends State<Home> {
           Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => const Favourites(),
+                builder: (context) => const Favourites(title: '',),
               ));
         },
         onCartPressed: () {
